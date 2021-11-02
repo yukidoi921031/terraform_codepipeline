c5c3b0ea35f0bdeeabcc3b64b76924b158e81f8b
yuki.doi <yuki.doi@ari-jp.com>
2021-11-02 10:29:51 +0900

    修正

--- _infra/tf_dryrun/dev/main.tf
+++ _infra/tf_dryrun/dev/main.tf
@@ -1,23 +1,37 @@
-module "plan" {
-  source = "../"
+provider "aws" {
+  region = "ap-northeast-1"
+}
 
-  github_token_ssm_key   = "GitHub_token_yukidoi921031"
+data "aws_ssm_parameter" "github_token" {
+  name = "GitHub_token_yukidoi921031"
+}
+
+provider "github" {
+  token = data.aws_ssm_parameter.github_token.value
+}
+
+module "plan" {
+  source = "../modules/codebuild"
+
+  github_token           = data.aws_ssm_parameter.github_token.value
   codebuild_role_name    = "codebuild_role_plan"
   codebuild_name         = "codebuild_plan"
   buildspec_file_name    = "terraform-plan"
   repository_url         = "https://github.com/yukidoi921031/terraform_codepipeline.git"
   environment            = "dev"
   webhook_filter_pattern = "PULL_REQUEST_CREATED"
+  tf_version             = "1.0.9"
 }
 
 module "apply" {
-  source = "../"
+  source = "../modules/codebuild"
 
-  github_token_ssm_key   = "GitHub_token_yukidoi921031"
+  github_token           = data.aws_ssm_parameter.github_token.value
   codebuild_role_name    = "codebuild_role_apply"
   codebuild_name         = "codebuild_apply"
   buildspec_file_name    = "terraform-apply"
   repository_url         = "https://github.com/yukidoi921031/terraform_codepipeline.git"
   environment            = "dev"
   webhook_filter_pattern = "PULL_REQUEST_MERGED"
+  tf_version             = "1.0.9"
 }
